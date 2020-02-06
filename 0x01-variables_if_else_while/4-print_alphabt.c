2938958c8bd1df440b1f71f7c733241422c5776c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-06 19:37:00 +0000

    Program to print alphabet without specific characters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Program to print alphabet without q and e
+  * alpha - variable
+  * Return: (0) Success
+  */
+int main(void)
+{
+	char alpha;
+	
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
