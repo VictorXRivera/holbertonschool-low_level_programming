b617d58ed277c90aa67dd72d1ea2013ace98b9e9
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-10 19:55:09 +0000

    Function to check for alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * _isalpha - function to check for alphabetic character
+  * (c) - parameter for function
+  * Return: 1
+  */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
