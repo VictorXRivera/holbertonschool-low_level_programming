6ac90312dc639bd0627675bbcb4f6ec65420ebb0
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-10 20:10:53 +0000

    Fix: corrected syntax for comment on parameter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
   * _islower - function that checks for lowercase characters
-  * c - parameter for function
+  * @c: parameter for function
   * Return: 0
   */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
-	else 
+	else
 		return (0);
 }
