4fa71077b1a49ce73657af5c7120948d62090744
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-11 14:41:32 +0000

    Function to print the last digit of given parameter

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,22 @@
 #include "holberton.h"
 
 /**
-  *
-  *
+  * print_last_digit - function to print last digit
+  * @n: parameter for function
+  * Return: lastdigit
   */
 int print_last_digit(int n)
 {
-	int lastdigit;
+	int lastdigit = n % 10;
 
-	lastdigit = n % 10;
+	if (lastdigit < 0)
+	{
+		lastdigit = lastdigit * -1;
+		_putchar(lastdigit + '0');
+		return (lastdigit);
+	}
+	else
+		_putchar(lastdigit + '0');
 
-	_putchar('0' + lastdigit);
-
-	return (lastdigit * -1);
+	return (lastdigit);
 }
