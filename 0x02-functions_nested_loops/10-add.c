df25e1d22d41cb8f59458ae220863f2d39a80938
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-12 13:29:26 +0000

    Function to add two integars

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
+
 /**
-  *
-  *
+  * add - function to add two integars and print the value
+  * @a: first parameter
+  * @b: second parameter
+  * Return: the result
   */
 int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
