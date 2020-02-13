8f93cde139dc2eec40108381390b4deaef06cf1a
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-13 18:28:49 +0000

    Function to print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * print_line - function to print underscore
+  * @n: parameter
+  * Return: success
+  */
+void print_line(int n)
+{
+	if (n > 0)
+		_putchar('_');
+
+	_putchar('\n');
+}
