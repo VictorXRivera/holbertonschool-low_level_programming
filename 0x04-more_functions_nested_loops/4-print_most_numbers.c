3f6f3ba5f6c47fccc4e50bea8f62410fc9367e00
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-13 14:44:23 +0000

    Function to print numbers and exclude two

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * print_most_numbers - function to print most numbers
+  * Return: success
+  */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
