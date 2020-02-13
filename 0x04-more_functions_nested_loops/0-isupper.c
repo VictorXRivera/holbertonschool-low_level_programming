dbfe11962293226d1035120d38415f7303fffeb0
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-13 14:22:54 +0000

    Function to check if c is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
-  *
-  *
+  * _isupper - function to check if variable is lowercase
+  * @c: parameter
+  * Return: 1 if lower, 0 if else
   */
 int _isupper(int c)
 {
