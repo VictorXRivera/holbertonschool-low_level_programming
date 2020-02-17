84b6b4075eb524cf0ac2f437ad4460428d84dbfb
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-17 16:00:00 +0000

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,17 @@
 int main(void)
 {
 	int n;
-
+	
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 15 == 0)
-			printf("FizzBuzz ");
+		if (n % 3 == 0)
+			printf("Fizz ");
 		else if (n % 5 == 0)
 			printf("Buzz ");
 		else if (n % 3 == 0)
 			printf("Fizz ");
-		else
-			printf("%d ", n);
 	}
+	printf("%d ", n);
 	printf("\n");
 	return (0);
 }
