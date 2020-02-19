8e1682711747eec4ddb341f4249c1adbf5fd8f21
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-19 22:12:04 +0000

    Function to copy string using pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index] != '\0')
+	while (*(src + index) != '\0')
 	{
-		*(dest + index) = *(src + index);
+		dest[index] = src[index];
 		index++;
 	}
 	return (dest);
