99a2035c822c7e59942a251805ef24ae90497127
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-20 19:12:18 +0000

    Function to cat two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * _strcat - function to cat two strings
+  * @dest: pointer parameter
+  * @src: pointer parameter
+  * Retun: Success
+  */
+char *_strcat(char *dest, char *src)
+{
+	int hello;
+	int world;
+
+	for (hello = 0; dest[hello] != '\0'; ++hello)
+	{}
+
+	for (world = 0; src[world] != '\0'; ++hello, ++world) 
+		dest[hello] = src[world];
+
+	dest[hello] = '\0';
+	return (dest);
+}
