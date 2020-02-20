cd6a201981645a00ae3d713ac3f93b8749bd5e36
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-20 19:41:38 +0000

    Function to copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,12 @@
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0;
+	int index;
 
-	while (index < n && src[index] != 0)
-	{
+	for (index = 0; index < n && src[index] != '\0'; index++)
 		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
+	for (; index < n; index++)
+		dest[index] = '\0';
+
 	return (dest);
 }
