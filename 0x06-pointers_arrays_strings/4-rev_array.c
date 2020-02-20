d0f2e3bd7b4661dafe0b6ecf8d6c97352163f80b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-20 22:35:15 +0000

    Function to reverse array using pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * reverse_array - function to reverse array
+  * @a: pointer
+  * @n: parameter
+  * Return: Success
+  */
+void reverse_array(int *a, int n)
+{
+	int index;
+	int rev;
+
+	for (index = 0; index < n / 2; index++, n--)
+	{
+		rev = a[index];
+		a[index] = a[n - 1];
+		a[n - 1] = rev;
+	}
+}
