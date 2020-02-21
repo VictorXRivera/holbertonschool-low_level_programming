1de1e8b9dc359d07017cf2ed8e0b97e80f88868f
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-21 21:12:35 +0000

    Function for leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,8 +2,22 @@
 
 /**
   * leet - function to encode
-  * @s: pointer parameter
+  * @str: pointer parameter
   * Return: Success
   */
-char *leet(char *s)
+char *leet(char *str)
 {
+	char leet [] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char change[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int count, index;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		for (index = 0; leet[index]; index++)
+		{
+			if (leet[index] == str[count])
+				str[count] = change[index];
+		}
+	}
+	return (str);
+}
