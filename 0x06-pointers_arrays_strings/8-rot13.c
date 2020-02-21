bc5cdc32fbacd8f7a6ab7329c1874a1569472801
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-21 21:26:32 +0000

    Rot 13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,3 +7,20 @@
   */
 char *rot13(char *s)
 {
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int index, dec;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		for (dec = 0; alphabet[dec]; dec++)
+		{
+			if (alphabet[dec] == s[index])
+			{
+				s[index] = rot13[dec];
+				break;
+			}
+		}
+	}
+	return (s);
+}
