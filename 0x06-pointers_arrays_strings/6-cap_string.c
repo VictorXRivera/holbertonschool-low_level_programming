a4397de2a33f12697ec6f299652cee67894b6624
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-21 22:58:42 +0000

    Function to cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,5 +7,31 @@
   */
 char *cap_string(char *s)
 {
+	int count;
+	char last_case;
+
+	for (count = 0, last_case = '.'; s[count] != '\0'; count++)
+	{
+		if (
+				last_case == ' ' ||
+				last_case == '\t' ||
+				last_case == '\n' ||
+				last_case == ',' ||
+				last_case == ';' ||
+				last_case == '.' ||
+				last_case == '!' ||
+				last_case == '?' ||
+				last_case == '"' ||
+				last_case == '(' ||
+				last_case == ')' ||
+				last_case == '{' ||
+				last_case == '}' ||
+				count == 0)
+		{
+			if (s[count] >= 'a' && s[count] <= 'z')
+				s[count] -= 32;
+		}
+		last_case = s[count];
+	}
 	return (s);
 }
