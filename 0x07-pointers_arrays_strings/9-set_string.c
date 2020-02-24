529236d067cbb3d472dd2ab5e70b8cb71037e3f7
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-24 20:49:09 +0000

    Set string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+  * set_string - function to set value of a pointer to a char
+  * @s: Double pointer
+  * @to: pointer
+  * Return: Nothing
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
