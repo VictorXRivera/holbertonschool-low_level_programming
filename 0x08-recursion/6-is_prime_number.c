15f7fd479d24711b60ded516449829aa96361e08
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-26 21:41:26 +0000

    Function to find prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * is_prime_number - function to find prime number
+  * @n: Integer parameter
+  * Return: 1
+  */
+int is_prime_number(int n)
+{
+	if (n % 2 > 1)
+		return (1);
+
+	return (0);
+}
