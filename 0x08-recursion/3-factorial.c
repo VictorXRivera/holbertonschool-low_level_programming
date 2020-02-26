c040413e01c71dcd95d7f2b802dd81a4c883a017
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-26 18:14:11 +0000

    Function to find factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
-  *
-  (
-  *
+  * factorial - Function to find factorial of num
+  * @n: integer parameter
+  * Return: Factorial
   */
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n < 0)
+		return (-1);
+
+	else if (n <= 1)
 		return (1);
-	
-	return (n * factorial(n-1));
+
+	return (n * factorial(n - 1));
 }
