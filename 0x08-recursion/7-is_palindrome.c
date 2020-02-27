041bd38c30fe9cce717b11efe8f4f4176609036a
VictorXRivera <victorxrivera.professional@gmail.com>
2020-02-27 00:22:34 +0000

    Function to check if string is palindrome

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+  * is_palindrome - function to check if string a palindrome
+  * @s: pointer to a string
+  * Return: 1 if palindrome, 0 if not
+  */
+int is_palindrome(char *s)
+{
+	return (1);
+}
