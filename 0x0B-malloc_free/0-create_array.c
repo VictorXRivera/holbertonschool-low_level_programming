fc3981ae3c5bebc908a937dbb2d1fd2ea4fb0759
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-03 21:23:57 +0000

    Function to create an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * create_array - function to create array of characters
+  * @size: unsigned int parameter
+  * @c: char parameter
+  * Return: Array
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int indexer;
+
+	array = malloc((size + 1) * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	indexer = 0;
+	while (indexer < size)
+	{
+		array[indexer] = c;
+		indexer++;
+	}
+	array[indexer] = '\0';
+	return (array);
+}
