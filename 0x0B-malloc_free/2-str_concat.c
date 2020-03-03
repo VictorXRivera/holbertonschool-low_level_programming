f6ee23f094b88b212969ea2f2f102a9fcfb1bf73
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-03 23:02:36 +0000

    Function to concat two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * str_concat - function to concat both strings
+  * @s1: pointer to first string
+  * @s2: pointer to second string
+  * Return: both strings
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *s = 0;
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (NULL);
+
+	return (s);
+}
