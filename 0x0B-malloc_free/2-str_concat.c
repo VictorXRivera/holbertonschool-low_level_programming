449e49c04e81792be3b9cb87006685a3abee9c53
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-04 21:34:19 +0000

    Function to concat string; has seg fault issue

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,35 @@
   */
 char *str_concat(char *s1, char *s2)
 {
-	char *s = 0;
+	int len1, len2 = 0; 
+	int counter1, counter2;
+	char *concat;
 
-	if (*s1 == '\0' && *s2 == '\0')
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
-	return (s);
+	while (s1[len1] != '\0')
+	{
+		counter1 = s1[len1];
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		counter2 = s2[len2];
+		len2++;
+	}
+	
+	if (counter1 == 0 || counter2 == 0)
+		return (NULL);
+
+	concat = (char *)malloc((counter1 + counter2 + 1) * sizeof(char));
+
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
+		concat[counter1] = s1[counter1];
+
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
+		concat[counter1] = s2[counter2];
+	
+	concat[counter1] = '\0';
+	return (concat);
 }
