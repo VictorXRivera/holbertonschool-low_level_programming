0fab1165db6ea55bee95ecea726d78122b10e5c4
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-05 18:50:51 +0000

    Function to concat string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,14 +11,13 @@
 char *str_concat(char *s1, char *s2)
 {
 	int len1 = 0, len2 = 0;
-	int counter1 = 0, counter2 = 0;
+	int counter;
 	char *concat;
 
 	if (s1 != NULL)
 	{
 		while (s1[len1] != '\0')
 		{
-			counter1 = s1[len1];
 			len1++;
 		}
 	}
@@ -26,20 +25,19 @@ char *str_concat(char *s1, char *s2)
 	{
 		while (s2[len2] != '\0')
 		{
-			counter2 = s2[len2];
 			len2++;
 		}
 	}
-	concat = malloc((counter1 + counter2 + 1) * sizeof(char));
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+	for (counter = 0; counter < len1; counter++)
+		concat[counter] = s1[counter];
+	for (counter = 0; counter < len2; counter++)
+		concat[len1 + counter] = s2[counter];
+
+	concat[len1 + len2 + 1] = '\0';
 
 	if (concat == NULL)
 		return (NULL);
-
-	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
-		concat[counter1] = s1[counter1];
-
-	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
-		concat[counter1 + counter2] = s2[counter2];
 
 	return (concat);
 }
