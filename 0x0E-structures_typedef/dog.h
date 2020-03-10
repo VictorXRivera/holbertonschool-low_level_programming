04e071515fb1367583561193ceb3f8b57243869d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-10 02:15:52 +0000

    Added more prototypes and headers to file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,16 +9,16 @@
   * @owner: pointer to name of owner
   * @age: age of dog
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
