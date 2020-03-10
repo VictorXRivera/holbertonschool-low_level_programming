ef84979ad230fa36a96ef159fc039be507419fbf
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-10 17:17:00 +0000

    Function to print new doggie

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include "dog.h"
+char *_strdup(char *str);
+
+/**
+  * new_dog - pointer to struct
+  * @name: pointer to name of dog
+  * @owner: pointer to name of owner
+  * @age: age of dog
+  * Return: Created new dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggie;
+	char *namecopy;
+	char *ownercopy;
+
+	doggie = malloc(sizeof(dog_t));
+	if (doggie == NULL)
+		return (NULL);
+
+	namecopy = _strdup(name);
+	ownercopy = _strdup(owner);
+
+	if (!namecopy || !ownercopy)
+	{
+		free(doggie);
+		free(namecopy);
+		free(ownercopy);
+		return (NULL);
+	}
+	doggie->name = namecopy;
+	doggie->owner = ownercopy;
+	doggie->age = age;
+
+	return(doggie);
+}
+char *_strdup(char *str)
+{
+	int original_size = 0;
+	char *duplicate_str;
+	char *dup_offset;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[original_size] != '\0')
+		original_size++;
+	original_size++;
+
+	duplicate_str = (char *)malloc(sizeof(char) * original_size);
+	if (duplicate_str == NULL)
+		return (NULL);
+
+	dup_offset = duplicate_str;
+	while (*str)
+	{
+		*dup_offset = *str;
+		dup_offset++;
+		str++;
+	}
+	*dup_offset = '\0';
+	return (duplicate_str);
+}
