b6c88cf8a874842b82726582d644c8fa0a6e2f62
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-10 02:23:41 +0000

    Added conditional to check if pointer is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,8 @@
   */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 }
