4483f94239d3647be2ec2b27318a33af177f1116
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-10 16:34:57 +0000

    Fix: defined new type dog_t correctly

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,14 @@
   * @owner: pointer to name of owner
   * @age: age of dog
   */
-typedef struct dog
+struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-} dog_t;
+};
 
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
