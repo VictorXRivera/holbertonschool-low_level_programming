efb2e8cecf5ce9b0ad118063adbd4f46ddfd772f
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-10 02:03:20 +0000

    Program to print input of django's info

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+  * print_dog - Program to print input
+  * @d: pointer to struct dog
+  * Return: Printed strings
+  */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
