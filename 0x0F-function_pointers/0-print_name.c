1c4c6171b676c362f012e483d6c33a2c7c43dbd7
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-11 14:34:18 +0000

    Fix: added Betty compliant comments to program

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 
 /**
-  *
-  *
-  *
+  * print_name - function to print name
+  * @name: Pointer to name being printed
+  * @f: pointer to function being called
+  * Return: Nothing
   */
 void print_name(char *name, void (*f)(char *))
 {
