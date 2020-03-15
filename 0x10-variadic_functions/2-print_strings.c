20a32e1736234f7a11dad76a710ee5dbcdacd671
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-15 15:54:02 +0000

    Fix: corrected conditionals

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,6 +10,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
+	char *ptr;
 
 	if (separator == NULL)
 		separator = "";
@@ -17,11 +18,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n && n != '\0'; i++)
 	{
-		printf("%s", va_arg(list, char *));
-		if (i < n - 1)
-		{
+		ptr = va_arg(list, char *);
+		if (!ptr)
+			printf("(nil)");
+		else
+			printf("%s", ptr);
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
 	va_end(list);
