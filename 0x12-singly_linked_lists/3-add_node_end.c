3a3643c9e6c5aff9cae5d90f2d2534116b6cb13f
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-20 18:33:16 +0000

    Function to add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,23 +12,25 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_end, *temp;
 	char *dup = strdup(str);
 
-	while (str[counter] != '\0')
-		++counter;
+	if (!str)
+		return (NULL);
 
 	new_end = malloc(sizeof(list_t));
 	if (new_end == NULL)
 	{
 		return (NULL);
 	}
-	temp = *head;
+	while (str[counter] != '\0')
+		++counter;
 	new_end->len = counter;
 	new_end->str = dup;
 	new_end->next = NULL;
+	temp = *head;
 
 	if (*head == NULL)
 	{
 		*head = new_end;
-		return (NULL);
+		return (*head);
 	}
 	while (temp->next != NULL)
 		temp = temp->next;
