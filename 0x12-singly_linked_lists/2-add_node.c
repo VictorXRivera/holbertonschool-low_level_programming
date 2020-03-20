c84cecfafc1108c437cf7aa648722a72e44741c0
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-20 13:58:08 +0000

    Fuction to add a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * add_node - iunction to add a node to the start of list
+  * add_node - function to add a node to the start of list
   * @head: start of the list, double pointer
   * @str: pointer to malloc'ed string
   * Return: Added node
@@ -11,10 +11,11 @@ list_t *add_node(list_t **head, const char *str)
 	int counter = 0;
 	list_t *temp;
 	char *dup = strdup(str);
+	temp = *head;
 
 	temp = malloc(sizeof(list_t));
 
-	if (!temp || !str)
+	if (!temp)
 		return (NULL);
 
 	while (dup[counter])
