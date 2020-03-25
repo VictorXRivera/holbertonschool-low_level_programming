e8d7934aba58ab50391ae03b6b060fc96eed28b2
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-25 16:47:58 +0000

    Function to insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,9 +12,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temp;
 	unsigned int counter;
 
-	if (!head)
-		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
@@ -23,19 +20,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = NULL;
 
 	temp = *head;
-	for (counter = 2; counter <= idx - 1; counter++)
+	if (idx == 0)
 	{
-		temp = temp->next;
-
-		if (temp == NULL)
-			return (NULL);
-	}
-	if (temp != NULL)
-	{
-		new_node->next = temp->next;
-		temp->next = new_node;
+		new_node->next = *head;
+		*head =new_node;
 		return (new_node);
 	}
-	else
-		return (NULL);
+	for (counter = 2; counter < idx - 1; counter++)
+	{
+		temp = temp->next;
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
