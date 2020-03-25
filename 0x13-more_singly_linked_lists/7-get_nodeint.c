5c515ba53b20220e44b3cd5d79239116ac0d0a15
VictorXRivera <victorxrivera.professional@gmail.com>
2020-03-25 13:11:59 +0000

    Function get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,22 +8,12 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *nth = head;
 	unsigned int counter = 0;
 
-	if (nth == NULL || head == NULL)
-		return (0);
-	if (!index)
-		return (NULL);
-
-	while (nth != NULL)
+	while (counter < index && head != NULL)
 	{
-		if (counter == index)
-		{
-			return (nth);
-		}
+		head = head->next;
 		counter++;
-		nth = nth->next;
 	}
-	return (nth);
+	return (head);
 }
