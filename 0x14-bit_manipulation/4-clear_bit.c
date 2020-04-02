d08c8c1153d1c125f7664c58376047428c5e782e
VictorXRivera <victorxrivera.professional@gmail.com>
2020-04-02 14:37:38 +0000

    Function to clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,9 +4,13 @@
   * clear_bit - function to clear bit
   * @n: pointer
   * @index: index of bits
-  * Return: Cleared bit or 0
+  * Return: 1 on success or 0 on error
   */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	return (*n & (~(1 << (index - 1))));
+	if (index >= 64)
+		return (-1);
+
+	*n = (*n & ~(1 << index));
+	return (1);
 }
