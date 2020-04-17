53b6ecce4ac4215019ecd3ffe32d678f4d2b1618
VictorXRivera <victorxrivera.professional@gmail.com>
2020-04-17 16:19:51 +0000

    Function to print natural numbers up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * print_to_98 - function to print up to 98
+  * @n: number
+  * Return: Nothing
+  */
+void print_to_98(int n)
+{
+	if (n == 98)
+		printf("%d\n", n);
+	else if (n < 98)
+	{
+		printf("%d, ", n);
+		print_to_98(n + 1);
+	}
+	else if (n > 98)
+	{
+		printf("%d, ", n);
+		print_to_98(n - 1);
+	}
+}
