5670a80111d488899166de18bb3dfc4185f5cb3e
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-02 22:15:24 +0000

    Function to find sum of list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function to add numbers in list
+ * @head: start of the list
+ * Return: Sum of list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *add = head;
+	int sum = 0;
+
+	while (add != NULL)
+	{
+		sum += add->n;
+		add = add->next;
+	}
+	return (sum);
+}	
