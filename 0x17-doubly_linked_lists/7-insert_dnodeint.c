700d6465b6fe515344a2118d44106ecaed04d133
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-03 17:21:32 +0000

    Function to insert nth node at given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h) = nth_node;
 		return (*h);
 	}
-	for (i = 1; i < idx ; i++)
+	for (i = 1; i < idx; i++)
 	{
 		temp = temp->next;
 	}
