eeaf8f5c32f9beab39e3f5d50d88634390cccba9
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-03 23:15:04 +0000

    Function to insert node at nth position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,27 +10,38 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *nth_node, *temp;
-	unsigned int i;
+
+	if ((*h) == NULL)
+		return (NULL);
 
 	nth_node = malloc(sizeof(dlistint_t));
 	if (nth_node == NULL)
-	{
 		return (NULL);
-	}
 	nth_node->n = n;
 	temp = (*h);
-	if ((*h) == NULL)
+	if (idx == 0)
 	{
-		nth_node->next = NULL;
-		(*h) = temp;
-		temp->prev = (*h);
-		return (temp);
+		nth_node->next = (*h);
+		if (*h)
+			(*h)->prev = nth_node;
+		(*h) = nth_node;
+		return (*h);
 	}
-	for (i = 1; i < idx; i++)
+	for (; idx > 1; idx--)
 	{
+		if (temp == NULL)
+		{
+			free(nth_node);
+			return (NULL);
+		}
 		temp = temp->next;
 	}
+	if (temp == NULL)
+		return (NULL);
 	nth_node->next = temp->next;
-	temp->next = nth_node;
+	nth_node->prev = temp;
+	if (temp->next)
+		nth_node->next->prev = nth_node;
+	nth_node->prev->next = nth_node;
 	return (nth_node);
 }
