1947bc23954099fd8fd30b1c1beeeb7c32ca83c7
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-31 17:55:18 +0000

    Hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,26 +9,40 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int hash, size;
+	unsigned long int hash;
 	hash_node_t *node;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || key[0] == '\0')
 		return (0);
 
-	size = ht->size;
-	hash = key_index((const unsigned char *)key, size);
-
-	if (ht->array[hash] != NULL && strcmp(ht->array[hash]->key, key) == 0)
+	hash = hash_djb2((const unsigned char *)key) % ht->size;
+	if (ht->array[hash] && strcmp(ht->array[hash]->key, (char *)key))
 	{
 		ht->array[hash]->value = strdup(value);
+		if (ht->array[hash]->value == NULL)
+			return (0);
 		return (1);
 	}
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
 	node->value = strdup(value);
+	if (node->value == NULL && value != NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
 	node->next = ht->array[hash];
 	ht->array[hash] = node;
+
 	return (1);
 }
