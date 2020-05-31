5bc8bffb9afb9e28a24e62ebf4aa0e42d7b915ae
VictorXRivera <victorxrivera.professional@gmail.com>
2020-05-31 18:58:31 +0000

    Getting functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,30 @@
-#include "calc.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-  *
-  *
-  *
-  */
+ * get_op_func - getting op functions
+ * @s: input
+ * Return: func
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
-    	};
-    	int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	if (s ! = "+", "-", "*", "/", "%" || !s)
-		return (NULL);
-	while (s)
+	while (i < 5)
 	{
-		s(get_op_func[i]);
+		if (*s == (*(ops + i)->op))
+			return (*(ops + i)->f);
+		i++;
 	}
-	return (i);
+	return (*(ops + i)->f);
 }
