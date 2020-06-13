8cfbb5d182ced8e1c64089e7f8317afc58fb3d11
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-13 12:41:21 +0000

    Just returned 98

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <sys/mman.h>
+#include <elf.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	return (98);
+}
